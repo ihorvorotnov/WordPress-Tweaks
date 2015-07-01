5e74c881fcac9dea8ebd54ef65233c3a8fa0e757
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2015-07-01 16:51:03 +0300

    Add empty plugin file with header

---
+++ wp-tweaks.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WordPress Tweaks
+Description: Some fixes, features and alternative defaults for WordPress.
+Author: Ihor Vorotnov
+Version: 0.1.0
+Author URI: http://ihorvorotnov.com
+*/
+
