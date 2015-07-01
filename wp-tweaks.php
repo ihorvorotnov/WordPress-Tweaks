2b64ee1dfc65b8306e67b5a25e67a8fad8a49901
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2015-07-01 16:52:06 +0300

    Introduce feature: remove Open Sans from Google Fonts

--- wp-tweaks.php
+++ wp-tweaks.php
@@ -7,3 +7,16 @@ Version: 0.1.0
 Author URI: http://ihorvorotnov.com
 */
 
+/**
+ * Remove Open Sans font from Google Fonts to speed-up admin (we have it installed locally).
+ * @since 0.1.0
+ */
+function wpt_remove_google_fonts() {
+
+	// Unload Open Sans
+	wp_deregister_style( 'open-sans' );
+	wp_register_style( 'open-sans', false );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpt_remove_google_fonts' );
+add_action( 'admin_enqueue_scripts', 'wpt_remove_google_fonts' );
